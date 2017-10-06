719b8a13bb8f4e9b228de19cab65a47a4faef519
Andreas Wilhelm <wilhelma@in.tum.de>
2017-10-06 10:09:05 +0200

    added TemplateHelper

---
+++ architecture/src/TemplateHelper.cpp
@@ -0,0 +1,36 @@
+//
+// Created by wilhelma on 10/4/17.
+//
+
+#include <string>
+
+#include "TemplateHelper.h"
+
+namespace pcv {
+
+TemplateHelper::TemplateHelper(Artifact_t *defaultArtifact)
+   : _defaultArtifact(defaultArtifact) {}
+
+Artifact_t* TemplateHelper::processRoutine(const Routine* routine)
+{
+  auto firstAngleBracket = routine->name.find('<');
+
+  if (firstAngleBracket != std::string::npos) {
+    auto pureRoutineName = routine->name.substr(0, firstAngleBracket);
+
+    // in case the templatized routine has not been seen so far => append it to the default artifact
+    if (_artifacts.find(pureRoutineName) == std::end(_artifacts)) {
+      _defaultArtifact->children.emplace_back(std::unique_ptr<pcv::Artifact_t> {
+         new pcv::Artifact_t(pureRoutineName, _defaultArtifact)
+      });
+      _artifacts[pureRoutineName] = _defaultArtifact->children.back().get();
+    }
+
+    return _artifacts[pureRoutineName];
+  } else {
+    return _defaultArtifact;
+  }
+}
+
+}
+
